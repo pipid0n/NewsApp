ef6e1af70cc50b05c8c31123298d1ab175c36089
pipid0n <iameugenemih@gmail.com>
2022-08-29 19:25:41 +0300

    added models for response, set retrofit

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org"
+        const val API_KEY = "a725cf87fc51408c849afd390e7d1d77"
+
+    }
+}
