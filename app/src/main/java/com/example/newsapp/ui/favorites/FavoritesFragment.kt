c7eace16547e94ffd4665fd0bb2b9d56dac912ca
pipid0n <iameugenemih@gmail.com>
2022-08-29 18:54:26 +0300

    added bottom navigation bar, set navigation, created fragments

---
+++ app/src/main/java/com/example/newsapp/ui/favorites/FavoritesFragment.kt
@@ -0,0 +1,25 @@
+package com.example.newsapp.ui.favorites
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsapp.R
+import com.example.newsapp.databinding.FragmentDetailsBinding
+import com.example.newsapp.databinding.FragmentFavoritesBinding
+
+class FavoritesFragment : Fragment() {
+
+    private var _binding: FragmentFavoritesBinding? = null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFavoritesBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/newsapp/ui/search/SearchFragment.kt
@@ -0,0 +1,26 @@
+package com.example.newsapp.ui.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newsapp.R
+import com.example.newsapp.databinding.FragmentDetailsBinding
+import com.example.newsapp.databinding.FragmentSearchBinding
+import com.example.newsapp.databinding.FragmentSearchBinding.*
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+}
